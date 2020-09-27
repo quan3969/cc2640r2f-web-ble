d5c227a853552768056bce616f59dd01185c69fe
Jason Zheng <quan3969@qq.com>
2020-09-27 16:52:23 +0800

    update

--- Simple Serial Socket Client/javascript.js
+++ Simple Serial Socket Client/javascript.js
@@ -4,108 +4,51 @@ let bleIsConnected = false;
 
 let bleServers = null;
 
-let ledStatus = [0, 0];
+let bleCharacteristics = null;
 
 window.onload = function(){
 
   let connectBtn = document.getElementById('connectBtn');
+   
   connectBtn.addEventListener('click', connectBtnClick);
 
-  let led1Btn = document.getElementById('led1Btn');
-  led1Btn.addEventListener('click', led1BtnClick);
+  let sendBtn = document.getElementById('sendBtn');
+  sendBtn.addEventListener('click', sendBtnClick);
 
-  let led2Btn = document.getElementById('led2Btn');
-  led2Btn.addEventListener('click', led2BtnClick);
+  let clearLogBoxBtn = document.getElementById('clearLogBoxBtn');
+  clearLogBoxBtn.addEventListener('click', clearLogBoxBtnClick);
+}
 
-  let btn1Btn = document.getElementById('btn1Btn');
-  btn1Btn.addEventListener('click', btn1BtnClick);
+function clearLogBoxBtnClick(){
+  document.getElementById("logBox").textContent = "";
+}
 
-  let btn2Btn = document.getElementById('btn2Btn');
-  btn2Btn.addEventListener('click', btn2BtnClick);
+function sendBtnClick(){
+  let toSendText = new ArrayBuffer;
+  console.log(document.getElementById('sendBox'));
+  return bleCharacteristics.writeValue(toSendText);
+}
 
+function handleInDataCharacteristicValueChanged(event){
+  let receivedString = String.fromCharCode.apply(null, new Uint8Array(event.target.value.buffer));
+  let receivedlen = receivedString.length;
+  document.getElementById("logBox").textContent += '(' + receivedlen + ') ' + receivedString + '\n';
 }
 
 function onDisconnected(event) {
   let device = event.target;
-  document.getElementById("blep").innerHTML = 'Device ' + device.name + ' is disconnected.';
-}
-
-function handleBtn1CharacteristicValueChanged(event){
-  var value = event.target.value.getUint8(0);
-  document.getElementById("btn1p").innerHTML = (value == 0) ? "Btn1 up." : "Btn1 down.";
-}
-
-function handleBtn2CharacteristicValueChanged(event){
-  var value = event.target.value.getUint8(0);
-  document.getElementById("btn2p").innerHTML = (value == 0) ? "Btn2 up." : "Btn2 down.";
-}
-
-function btn2BtnClick(){
-  return bleServers[1].getCharacteristics()
-  .then(characteristics => {
-    characteristics[1].startNotifications();
-    characteristics[1].addEventListener('characteristicvaluechanged', handleBtn2CharacteristicValueChanged);
-  });
-}
-
-function btn1BtnClick(){
-  return bleServers[1].getCharacteristics()
-  .then(characteristics => {
-    characteristics[0].startNotifications();
-    characteristics[0].addEventListener('characteristicvaluechanged', handleBtn1CharacteristicValueChanged);
-  });
-}
-
-function led2BtnClick(){
-  if (ledStatus[1] == 0)
-  {
-    ledStatus[1] = 1;
-    document.getElementById("led2p").innerHTML = (ledStatus[1] == 1) ? "Led1 is on." : "Led1 is off.";
-    return bleServers[0].getCharacteristics()
-    .then(characteristics => {
-      return characteristics[1].writeValue(Uint8Array.of(1));
-    });
-  } else{
-    ledStatus[1] = 0;
-    document.getElementById("led2p").innerHTML = (ledStatus[1] == 1) ? "Led1 is on." : "Led1 is off.";
-    return bleServers[0].getCharacteristics()
-    .then(characteristics => {
-      return characteristics[1].writeValue(Uint8Array.of(0));
-    });
-  }
-}
-
-function led1BtnClick(){
-  if (ledStatus[0] == 0)
-  {
-    ledStatus[0] = 1;
-    document.getElementById("led1p").innerHTML = (ledStatus[0] == 1) ? "Led1 is on." : "Led1 is off.";
-    return bleServers[0].getCharacteristics()
-    .then(characteristics => {
-      return characteristics[0].writeValue(Uint8Array.of(1));
-    });
-  } else{
-    ledStatus[0] = 0;
-    document.getElementById("led1p").innerHTML = (ledStatus[0] == 1) ? "Led1 is on." : "Led1 is off.";
-    return bleServers[0].getCharacteristics()
-    .then(characteristics => {
-      return characteristics[0].writeValue(Uint8Array.of(0));
-    });
-  }
+  document.getElementById("logBox").textContent += 'Device ' + device.name + ' is disconnected.\n';
 }
 
 function connectBtnClick(){
   navigator.bluetooth.requestDevice({
-    // acceptAllDevices: true
     filters: [{ 
-      name: 'ProjectZero',
-      // services: ['heart_rate']
+      name: 'SimpleSerialSocket',
     }],
-    optionalServices: [ 0x1110, 0x1120 ] 
+    optionalServices: [ 0xC0C0 ] 
   })
   .then(device => {
-    // console.log(device);
-    document.getElementById("blep").innerHTML = 'Connected to device: ' + device.name;
+    document.getElementById("logBox").textContent += 'Connected to device: ' + device.name + ".\n";
     // Set up event listener for when device gets disconnected.
     device.addEventListener('gattserverdisconnected', onDisconnected);
     // Attempts to connect to remote GATT Server.
@@ -121,13 +64,14 @@ function connectBtnClick(){
     return servers[0].getCharacteristics();
   })
   .then(characteristics => {
-    ledStatus[0] = characteristics[0].readValue().then(value => {value.getUint8()});
-    ledStatus[1] = characteristics[1].readValue().then(value => {value.getUint8()});
+    characteristics[1].startNotifications();
+    characteristics[1].addEventListener('characteristicvaluechanged', handleInDataCharacteristicValueChanged);
+    // console.log(characteristics);
+    bleCharacteristics = characteristics[0];
+    // console.log(bleCharacteristics);
   })
   .then(() => {
-    document.getElementById("led1p").innerHTML = (ledStatus[0] == 1) ? "Led1 is on." : "Led1 is off.";
-    document.getElementById("led2p").innerHTML = (ledStatus[1] == 1) ? "Led2 is on." : "Led2 is off.";
-    console.log("all ready");
+    document.getElementById("logBox").textContent += 'All ready.\n';
   })
   .catch(error => { console.log(error); });
 }
