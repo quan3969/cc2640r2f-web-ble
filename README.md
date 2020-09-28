e3ae5c4d29026765c5315bc6c97482ba7ef27dd9
Jason Zheng <quan3969@qq.com>
2020-09-28 16:18:10 +0800

    Modify files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# projectZeroWebBleApp
-A web bluetooth APP built to interact with cc2640r2f ble5_projectZero.
+# Web Bluetooth Application for CC2640R2F
+此库包含多个用 Web Bluetooth API 编写的网页版上位机，用来与 CC2640R2F 设备进行互动。
+
+需要浏览器支持 Web Bluetooth API，同时设备带蓝牙设备。
+
+Project Zero：
+
+可与 TI 官方例程 Project Zero 互动，控制 LED，监测按键状态。
+
+Simple Serial Socket Client
+
+可与 TI 官方例程 Simple Serial Socket Server 互动，实现串口信息收发。
--- Simple Serial Socket Client/javascript.js
+++ Simple Serial Socket Client/javascript.js
@@ -6,6 +6,8 @@ let bleServers = null;
 
 let bleCharacteristics = null;
 
+// 打开网页时首先加载此函数，做初始化工作
+// 声明按键的回调函数，未连接蓝牙前禁用发送按键
 window.onload = function(){
 
   let connectBtn = document.getElementById('connectBtn');
@@ -17,61 +19,130 @@ window.onload = function(){
 
   let clearLogBoxBtn = document.getElementById('clearLogBoxBtn');
   clearLogBoxBtn.addEventListener('click', clearLogBoxBtnClick);
+  
+  document.getElementById('sendBtn').disabled = true;
 }
 
+// ArrayBuffer 转换成 String
+function ab2str(buf) {
+
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
+
+}
+
+// String 转换成 ArrayBuffer
+function str2ab(str) {
+
+  var buf = new ArrayBuffer(str.length);
+  var bufView = new Uint8Array(buf);
+
+  for (var i=0, strLen=str.length; i<strLen; i++) {
+
+    bufView[i] = str.charCodeAt(i);
+
+  }
+
+  return buf;
+}
+
+// 清空 Log 文字框
 function clearLogBoxBtnClick(){
+
   document.getElementById("logBox").textContent = "";
+
 }
 
+// 发送按键事件
+// 读取输入框内容，转换成 ArrayBuffer。
+// 通过蓝牙发送出去，并在 Log 文本框中显示，同时 Log 文本框滚动到最后一行
 function sendBtnClick(){
-  let toSendText = new ArrayBuffer;
-  console.log(document.getElementById('sendBox'));
-  return bleCharacteristics.writeValue(toSendText);
+
+  var toSendString = document.getElementById('sendBox').value;
+  document.getElementById("logBox").textContent += 'Out(' + toSendString.length + '): ' + toSendString + '\n';
+
+  let logBox = document.getElementById('logBox');
+  logBox.scrollTop = logBox.scrollHeight;
+
+  return bleCharacteristics.writeValue(str2ab(toSendString));
 }
 
+// 蓝牙通知事件
+// 将通知中的文字读取，并转换成 String
+// 在 Log 文本框中显示，同时 Log 文本框滚动到最后一行
 function handleInDataCharacteristicValueChanged(event){
-  let receivedString = String.fromCharCode.apply(null, new Uint8Array(event.target.value.buffer));
-  let receivedlen = receivedString.length;
-  document.getElementById("logBox").textContent += '(' + receivedlen + ') ' + receivedString + '\n';
+
+  let receivedString = ab2str(event.target.value.buffer);
+  document.getElementById("logBox").textContent += 'In (' + receivedString.length + '): ' + receivedString + '\n';
+
+  let logBox = document.getElementById('logBox');
+  logBox.scrollTop = logBox.scrollHeight;
 }
 
+// 蓝牙连接断开事件
+// 断开连接后，禁用发送按键
+// 在 Log 文本框中显示，同时 Log 文本框滚动到最后一行
 function onDisconnected(event) {
+
+  document.getElementById('sendBtn').disabled = true;
+
   let device = event.target;
   document.getElementById("logBox").textContent += 'Device ' + device.name + ' is disconnected.\n';
+
+  let logBox = document.getElementById('logBox');
+  logBox.scrollTop = logBox.scrollHeight;
 }
 
+// 搜索蓝牙设备，过滤无关设备，最后连接具有相关服务的蓝牙设备
+// 连接后将蓝牙服务存储为全局变量，方便后续使用
+// 一切准备就绪后，使能发送按键
+// 在 Log 文本框中显示，同时 Log 文本框滚动到最后一行
 function connectBtnClick(){
+
   navigator.bluetooth.requestDevice({
+
     filters: [{ 
       name: 'SimpleSerialSocket',
     }],
-    optionalServices: [ 0xC0C0 ] 
+    optionalServices: [ 0xC0C0 ]
+
   })
   .then(device => {
+
     document.getElementById("logBox").textContent += 'Connected to device: ' + device.name + ".\n";
+
+    let logBox = document.getElementById('logBox');
+    logBox.scrollTop = logBox.scrollHeight;
+
     // Set up event listener for when device gets disconnected.
     device.addEventListener('gattserverdisconnected', onDisconnected);
+
     // Attempts to connect to remote GATT Server.
     return device.gatt.connect();
   })
   .then(server => {
-    // console.log(server);
+
     return server.getPrimaryServices();
+
   })
   .then(servers =>{
-    // console.log(servers);
+
     bleServers = servers;
     return servers[0].getCharacteristics();
   })
   .then(characteristics => {
+
     characteristics[1].startNotifications();
     characteristics[1].addEventListener('characteristicvaluechanged', handleInDataCharacteristicValueChanged);
-    // console.log(characteristics);
     bleCharacteristics = characteristics[0];
-    // console.log(bleCharacteristics);
   })
   .then(() => {
+
+    document.getElementById('sendBtn').disabled = false;
+
     document.getElementById("logBox").textContent += 'All ready.\n';
+    
+    let logBox = document.getElementById('logBox');
+    logBox.scrollTop = logBox.scrollHeight;
   })
   .catch(error => { console.log(error); });
 }
